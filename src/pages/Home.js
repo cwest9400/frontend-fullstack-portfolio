7525d41d0a88332c64b9838a82be78f991a3dbdf
cwest9400 <chrstphrwest@gmail.com>
2023-01-07 23:04:25 -0500

    content added to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
+import headshot from "../images/headshot.png"
+
 function Home(props) {
     return (
-        <section className="home">
+        <section className="home-section">
             <div className="intro-card">
                 <h1>I'm a software engineer who is passionate<br/>about turning obstacles into opportunity,</h1>
                 <h3>focusing on the human stuck in the middle.</h3>
-                <img src="public/images/headshot zoomout square.png"/>
+                <img src={headshot}/>
             </div>
         </section>
     )
