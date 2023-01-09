e01fac95b6122429a18fb38fb5843c064c28c181
cwest9400 <chrstphrwest@gmail.com>
2023-01-09 08:56:03 -0500

    styling home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,11 @@ function Home(props) {
     return (
         <section className="home-section">
             <div className="intro-card">
-                <h1>I'm a software engineer who is passionate<br/>about turning obstacles into opportunity,</h1>
-                <p>focusing on the human stuck in the middle.</p>
+                <p className="p1">I'm a software engineer & designer who is passionate <br/>about turning obstacles into opportunity,</p>
+                <p className="p2">focusing on the human stuck in the middle.</p>
+                <div className="img-headshot">
                 <img src={headshot}/>
+                </div>
             </div>
         </section>
     )
