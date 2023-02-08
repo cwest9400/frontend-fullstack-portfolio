c1b41cacbda3d78a0df7296f9ebb901635cc51f8
cwest9400 <chrstphrwest@gmail.com>
2023-02-08 16:02:31 -0500

    project card tweak

--- src/images/projectImg.js
+++ src/images/projectImg.js
@@ -2,8 +2,8 @@
 
 const PROJECT_IMG = {
     4: "https://i.imgur.com/tD4kObG.png",
-    3: "https://i.imgur.com/tD4kObG.png",
-    2: "https://i.imgur.com/tD4kObG.png",
+    3: "https://i.imgur.com/Z84fXa4.png",
+    2: "https://i.imgur.com/BcC9Y8B.png",
 
 }
 export default PROJECT_IMG
