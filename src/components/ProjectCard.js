00bed06d8eb8d4bc1c5531e86639ee2629a9b6e0
cwest9400 <chrstphrwest@gmail.com>
2023-02-15 14:46:03 -0500

    mobile resize

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -7,21 +7,16 @@ export default function ProjectCard(props) {
     return (
         <div className="project-card">
             <div className="project-description">
-                <h1 className="project-card-title">{props.name}</h1>
-                <div>{props.type}</div>
+                <div className="project-card-title">{props.name} - <span className="project-type-paragraph">{props.type}</span></div>
                 <div className="project-desc-paragraph">{props.desc} </div>
                 <div className="project-technology-title">technology</div>
                 <div className="project-technology-container">
                     {props.tech}
                     </div>
-                
-     
                 <div className="project-button-container">
-                    
                     <a className="button-project-links" href={props.git} target="_blank">
                         gitHub
                     </a>
-                    
                     <a className="button-project-links" href={props.live} target="_blank">
                         live site
                     </a>
