44932f8377d6ee9d29eee02bd8aa43a70cff59ef
cwest9400 <chrstphrwest@gmail.com>
2024-05-22 23:31:48 -0400

    update info

--- src/images/projectImg.js
+++ src/images/projectImg.js
@@ -1,8 +1,8 @@
 
 
 const PROJECT_IMG = {
-    1: "https://i.imgur.com/4pBzEFJ.png",
-    3: "https://i.imgur.com/YLkZoWo.png",
+    3: "https://i.imgur.com/4pBzEFJ.png",
+    1: "https://i.imgur.com/YLkZoWo.png",
     2: "https://i.imgur.com/BcC9Y8B.png",
 
 }
